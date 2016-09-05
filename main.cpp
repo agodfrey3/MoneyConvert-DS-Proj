47d368d4dc6c2e99628aca44f15e9146e9f5f449
agodfrey3 <agodfrey3@fordham.edu>
2016-09-04 20:14:19 -0400

    Improved input reciever
    Added modularization, as well as fixed input bugs. Many input errors have been tested, and the program has handled them all well. The input is now working as expected. Further bugs may arise, however, will be dealt with accordingly.

--- main.cpp
+++ main.cpp
@@ -1,66 +1,96 @@
+//Andrew Godfrey
+//Money Value to English Expression
+//Last Updated: 9/4/2016
+//Known Bug:
 #include <iostream>
+#include <string>
 using namespace std;
+
+char getInput(int &dollars, int &cents);
+void inputStatus(char exitCode);
+void displayValues(int dollars, int cents);
 
 int main()
 {
-         int  dollars, cents, thousands, hundreds, tens, ones, tenths, hundreths;
-         char ch, exitCode;
-         bool isGood;
- 
-          cout << "-----Welcome to MakeMoney-----\n"
-               << "Enter an amount to convert: $";
-          while(true)//Creating main loop to take input ( will be placed into a function later)
-          {
-                  cin >> dollars;
-                  if (cin.fail() || dollars > 9999)//Checks dollar amount to ensure validity
-                  {
-                          cin.clear();
-                          cin.ignore();
-                          exitCode = 'd';//exit code for invalid dollar 
-                          break;
-                  }
-                  cin >> ch;
-                  if (cin.fail() || ch != '.')//checks to make sure there is a period separating the two values
-                  {
-                          cin.clear();
-                          cin.ignore();
-                          exitCode = 's';//exit code for separation error
-                          break;
-                  }
-                  cin >> cents;
-                  if (cin.fail() || cents > 99)//checks cents for validity
-                  {
-                          cin.clear();
-                          cin.ignore();
-                          exitCode = 'c';//exit code for cent error
-                          break;
-                  }
-                  exitCode = 'g';//exit code for clean entry
-                  break;
-          }
- 
-          switch (exitCode)//returns a message based on the exitCode from initial input function
-          {
-                  case ('d'):
-                          cout << "You have entered an invlid input to the dollars place.\n"
-                               << "\tPlease try again.\n";
-                          break;
-                  case ('s'):
-                          cout << "Error: You need to separate the dollar amount by a period.\n"
-                               << "\tPlease try again.\n";
-                          break;
-                  case ('c'):
-                          cout << "You have entered an invalid input to the cents place\n"
-                               << "\tPlease try again\n";
-                          break;
-                  case ('g'):
-                          cout << "Thank you. Your input was valid.\n"
-                               << "\tProcessing...\n";
-                          break;
-                  deafult:
-                          cout << "Bug in code. Check over logic\n";
-                          break;
-          }
-          return 0;
-  }
- 
+	int  dollars, cents;
+	char exitCode;
+
+	do//gets input until the input satisfies conditions
+	{
+		cout << "----------------Welcome to MakeMoney----------------\n"
+			<< "Please enter an amount between 9999.99 and 0.00\n"
+			<< "Enter an amount to convert: $";
+		exitCode = getInput(dollars, cents);//gets input and returns a code connected to an error or successful run
+		inputStatus(exitCode);// Outputs whether the input was valid or not
+	} while (exitCode != 'g');
+
+	displayValues(dollars, cents);
+
+	return 0;
+}
+
+char getInput(int &dollars, int &cents)
+{
+	int dol, cen;
+	char ch;
+	char code = 'g';
+	while (true)
+	{
+		cin >> dol;
+		if (cin.fail() || dol > 9999)//get input for dollars and checks validity
+		{
+			code = 'e';
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			break;
+		}
+
+		cin >> ch;
+		if (cin.fail() || ch != '.')//gets input for separater and checks validity
+		{
+			code = 'e';
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			break;
+		}
+
+		cin >> cen;
+		if (cin.fail() || cen > 99)//gets input for cents and checks validity 
+		{
+			code = 'e';
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			break;
+		}
+		break;
+	}
+
+	if (code == 'g')//uses param variables to change the main functions dollar and cents main variables if the input was good
+	{
+		dollars = dol;
+		cents   = cen;
+	}
+	return code;
+}
+
+void inputStatus(char exitCode)
+{
+	switch (exitCode)
+	{
+	case ('e') ://code for error
+		cout << "Your input was invalid, please try again.\n\n";
+		break;
+	case ('g') ://code for good input
+		cout << "Thank you. Your input was valid.\n"
+		<< "\tProcessing...\n\n";
+		break;
+	default://code if the getInput function malfunctioned
+		cout << "Bug in code. Check over logic\n";
+		break;
+	}
+}
+
+void displatValues(int dollars, int cents)
+{
+
+}
